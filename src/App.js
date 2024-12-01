ec59c45b8e436e8af11a08299d0f8ba673f6b9c1
Onyinye Oguocha <stephanieoguocha@gmail.com>
2024-12-01 16:25:54 +0100

    Add components styling

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import './App.css';
-import Home from './components/Homepage';
-import Nav from './components/Nav'
-import Main from './components/Main';
-import ConfirmedBooking from "./components/ConfirmedBooking"; // The new confirmation component
-
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Pages/Homepage";
+import About from "./components/Pages/AboutPage";
+import Menu from "./components/Pages/MenuPage";
+import Order from "./components/Pages/OrderPages";
+import Login from "./components/Pages/LoginPage";
+import BookingPage from './components/Pages/booking/BookingPage'
+import ConfirmedBooking from "./components/Pages/booking/ConfirmedBooking";
+import Layout from "./components/layout/Layout";
 
 function App() {
   return (
     <>
-    <Router>
-      <Nav /> {/* Use the Nav component here */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<AboutUs />} /> */}
-        {/* <Route path="/menu" element={<Menu />} /> */}
-        <Route path="/reservations" element={<Main />} />
-        {/* <Route path="/order" element={<Order />} /> */}
-        {/* <Route path="/login" element={<Login />} /> */}
-        <Route path="/confirmed" element={<ConfirmedBooking />} />
-      </Routes>
-    </Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/reservations" element={<BookingPage />} />
+          <Route path="/order" element={<Order />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/confirmed" element={<ConfirmedBooking />} />
+        </Routes>
+      </Layout>
     </>
   );
 }
